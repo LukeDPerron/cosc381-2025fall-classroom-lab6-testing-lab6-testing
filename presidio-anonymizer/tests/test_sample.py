c503713971445bc270c61005b887d16f51e77c43
Luke Perron <LukeDPerron@users.noreply.github.com>
2025-10-01 02:13:22 +0000

    testing sample.py

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -3,4 +3,19 @@ from presidio_anonymizer.sample import sample_run_anonymizer
 
 def test_sample_run_anonymizer():
     # replace the following line with your test
+    expected_outcome = {
+    'text': 'My name is BIP.',
+     'items':
+     [
+       {'start': 11, 'end': 14, 'entity_type': 'PERSON', 'text': 'BIP', 'operator': 'replace'}
+     ]
+    }
+
+    assert sample_run_anonymizer("My name is Bond.", 11, 15).text == expected_outcome["text"]
+    assert sample_run_anonymizer("My name is Bond.", 11, 15).items[0].start == expected_outcome["items"][0]["start"]
+    assert sample_run_anonymizer("My name is Bond.", 11, 15).items[0].end == expected_outcome["items"][0]["end"]
+    assert sample_run_anonymizer("My name is Bond.", 11, 15).items[0].entity_type == expected_outcome["items"][0]["entity_type"]
+    assert sample_run_anonymizer("My name is Bond.", 11, 15).items[0].text == expected_outcome["items"][0]["text"]
+    assert sample_run_anonymizer("My name is Bond.", 11, 15).items[0].operator == expected_outcome["items"][0]["operator"]
+
     pass
