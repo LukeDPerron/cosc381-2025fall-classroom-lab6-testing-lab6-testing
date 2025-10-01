acde3583e61a50b0533b30a8cd9a8c21b87d8a1e
Luke Perron <LukeDPerron@users.noreply.github.com>
2025-10-01 02:45:52 +0000

    added result variable

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -10,12 +10,14 @@ def test_sample_run_anonymizer():
        {'start': 11, 'end': 14, 'entity_type': 'PERSON', 'text': 'BIP', 'operator': 'replace'}
      ]
     }
+    
+    result = sample_run_anonymizer("My name is Bond.", 11, 15)
 
-    assert sample_run_anonymizer("My name is Bond.", 11, 15).text == expected_outcome["text"]
-    assert sample_run_anonymizer("My name is Bond.", 11, 15).items[0].start == expected_outcome["items"][0]["start"]
-    assert sample_run_anonymizer("My name is Bond.", 11, 15).items[0].end == expected_outcome["items"][0]["end"]
-    assert sample_run_anonymizer("My name is Bond.", 11, 15).items[0].entity_type == expected_outcome["items"][0]["entity_type"]
-    assert sample_run_anonymizer("My name is Bond.", 11, 15).items[0].text == expected_outcome["items"][0]["text"]
-    assert sample_run_anonymizer("My name is Bond.", 11, 15).items[0].operator == expected_outcome["items"][0]["operator"]
+    assert result.text == expected_outcome["text"]
+    assert result.items[0].start == expected_outcome["items"][0]["start"]
+    assert result.items[0].end == expected_outcome["items"][0]["end"]
+    assert result.items[0].entity_type == expected_outcome["items"][0]["entity_type"]
+    assert result.items[0].text == expected_outcome["items"][0]["text"] 
+    assert result.items[0].operator == expected_outcome["items"][0]["operator"]
 
     pass
